8b1c805a4caf9875a2d77164cee8965d8f76191c
Jacobo <tadeo.jacobo6@gmail.com>
2024-06-07 22:58:46 -0400

    starting api

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 const app = express();
+import diaryRouter from './routes/diaries';
 
 app.use(express.json()); // for parsing application/json
 
@@ -10,6 +11,8 @@ app.get('/', (_req, res) => {
     
   res.send('Hello World!');
 });
+
+app.use('/api/diaries', diaryRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
