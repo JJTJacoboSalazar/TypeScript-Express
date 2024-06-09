a12da9a158ef5a71bd4b7820268e96871d8fd0b2
Jacobo <tadeo.jacobo6@gmail.com>
2024-06-08 23:27:59 -0400

    Types and interfaces

---
+++ src/types.d.ts
@@ -0,0 +1,15 @@
+export type Weather = 'sunny' | 'rainy' | 'cloudy' | 'stormy' | 'snowy' | 'windy' | 'foggy' | 'hailing' | 'thunderstorm' | 'tornado';
+export type Visibility = 'great' | 'good' | 'ok' | 'poor';
+
+export interface DiaryEntry {
+    id: number;
+    date: string;
+    weather: Weather;
+    visibility: Visibility;
+    comment: string;
+}
+
+// Add a new interface SpecialDiaryEntry that extends DiaryEntry and has a new property flightNumber.
+interface SpecialDiaryEntry extends DiaryEntry {
+    flighNumber: number;
+}
