11c38ab33c018b0c3b0a09554214ad635b90465c
Jacobo <tadeo.jacobo6@gmail.com>
2024-06-11 16:28:12 -0400

    pick

--- src/services/diaryServices.ts
+++ src/services/diaryServices.ts
@@ -1,9 +1,13 @@
-import { DiaryEntry } from '../types';
-import diaryData from './diaries.json'
+import { DiaryEntry, NonSensitiveDiaryEntry } from '../types';
+import diaryData from './diaries.json';
 
 const diaries: Array<DiaryEntry> = diaryData as Array<DiaryEntry>;
 
-export const getEntries = () => diaries;
+export const getEntries = (): Array<DiaryEntry> => {
+  return diaries;
+}
+
+export const getEntriesWithoutSensitiveInfo = (): NonSensitiveDiaryEntry[] => diaries
 
 export const addEntry = () => {
   // TODO
--- src/types.d.ts
+++ src/types.d.ts
@@ -2,14 +2,16 @@ export type Weather = 'sunny' | 'rainy' | 'cloudy' | 'stormy' | 'snowy' | 'windy
 export type Visibility = 'great' | 'good' | 'ok' | 'poor';
 
 export interface DiaryEntry {
-    id: number;
-    date: string;
-    weather: Weather;
-    visibility: Visibility;
-    comment: string;
+    id: number
+    date: string
+    weather: Weather
+    visibility: Visibility
+    comment: string
 }
 
 // Add a new interface SpecialDiaryEntry that extends DiaryEntry and has a new property flightNumber.
 // interface SpecialDiaryEntry extends DiaryEntry {
 //     flighNumber: number;
 // }
+
+export type NonSensitiveDiaryEntry = Pick<DiaryEntry, 'id' | 'date' | 'weather' | 'visibility'>;
