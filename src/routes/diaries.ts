5c0db2c5f24a43fcfefa0742ad56559958bbe8b3
Jacobo <tadeo.jacobo6@gmail.com>
2024-06-12 12:07:21 -0400

    Api running

--- src/routes/diaries.ts
+++ src/routes/diaries.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import * as diaryService from '../services/diaryServices';
+
 const router = express.Router();
 
 router.get('/', (_req, res) => {
-  res.send('Fetching all diaries!');
+  res.send(diaryService.getEntriesWithoutSensitiveInfo());
 });
 
 router.post('/', (_req, res) => {
